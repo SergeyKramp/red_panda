20caacdcccee356c06cc7bd8673331799a397c84
sergeykramp <sergey@giftomatic.io>
2026-02-21 20:53:40 +0000

    Create a course grid

--- frontend/src/features/api/api.ts
+++ frontend/src/features/api/api.ts
@@ -1,6 +1,18 @@
 /**
  * Shared API utilities and types.
  */
+import z from "zod";
 
 export const API_BASE_URL =
   process.env.REACT_BASE_URL ?? "http://localhost:8080";
+
+export const CourseCardInfoZod = z.object({
+  code: z.string().min(1),
+  name: z.string().min(1),
+  credits: z.number().int().nonnegative(),
+  specialization: z.string().min(1),
+});
+
+export const CourseCardInfoListZod = z.array(CourseCardInfoZod);
+
+export type CourseCardInfo = z.infer<typeof CourseCardInfoZod>;
--- frontend/src/features/ui/index.ts
+++ frontend/src/features/ui/index.ts
@@ -1,3 +1,4 @@
 export * from "./layout";
 export * from "./navbar";
 export * from "./course-card";
+export * from "./course-grid";
